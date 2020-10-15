e7a3c12eaa4323b118cf5953d1fc0241899300e1
2jc26 <javier.cerino.p@gmail.com>
2020-10-14 21:50:49 -0500

    Pajaro2
    Pajaro2

--- intentoTobogánBola/Assets/Scripts/LVLPajaro/Personaje.cs
+++ intentoTobogánBola/Assets/Scripts/LVLPajaro/Personaje.cs
@@ -13,30 +13,45 @@ public class Personaje : MonoBehaviour
     public float jumpHeight=8f;
     public bool isGrounded;
 
-
+    private float inicioDisparo;
+    public float tiempoDisparo;
+    public float VelDisparo;
+    public Transform lanzador;
+    public Rigidbody flechaPrefab;
     void Start() {
         isGrounded=false;
     }
 
     void FixedUpdate() {
-        transform.Rotate(0,x*Time.deltaTime*velRotacion,0);
-        transform.Translate(0,0,y*Time.deltaTime*velMovimiento);
+            transform.Rotate(0,x*Time.deltaTime*velRotacion,0);
+            transform.Translate(0,0,y*Time.deltaTime*velMovimiento);
+
     }
 
     void Update()
     {
         x= Input.GetAxis("Horizontal");
         y= Input.GetAxis("Vertical");
+        
+        if(Input.GetMouseButtonDown(0) && isGrounded && Time.time > inicioDisparo){
+            anim.SetTrigger("Golpe");
+            inicioDisparo = Time.time + tiempoDisparo;
+            Rigidbody flechaPrefabInstanc;
+
+            flechaPrefabInstanc = Instantiate(flechaPrefab, lanzador.position, Quaternion.identity) as Rigidbody;
+            flechaPrefabInstanc.AddForce(lanzador.forward*100*VelDisparo);
+        }
+
         anim.SetFloat("VelX",x);
         anim.SetFloat("VelY",y);
 
 
-        if (isGrounded){
 
-            if(Input.GetKeyDown(KeyCode.Space)){
-                anim.SetBool("Jump",true);
-                rb.AddForce(new Vector3(0,jumpHeight,0),ForceMode.Impulse);
-            }
+        if (isGrounded){
+                if(Input.GetKeyDown(KeyCode.Space)){
+                    anim.SetBool("Jump",true);
+                    rb.AddForce(new Vector3(0,jumpHeight,0),ForceMode.Impulse);
+                }
             anim.SetBool("Grounding",true);
         }else{
             Falling();
