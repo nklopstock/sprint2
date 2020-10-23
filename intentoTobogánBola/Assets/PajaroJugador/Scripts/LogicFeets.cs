6928edb5b13546b0ebe4f8f2a18c6f532b8f81de
2jc26 <javier.cerino.p@gmail.com>
2020-10-23 10:49:54 -0500

    Pajaro

---
+++ intentoTobogánBola/Assets/PajaroJugador/Scripts/LogicFeets.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicFeets : MonoBehaviour
+{
+    public Personaje player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other) {
+        player.isGrounded = true;
+    }
+    
+    private void OnTriggerExit(Collider other) {
+        player.isGrounded = false;
+    }
+}
---
+++ intentoTobogánBola/Assets/PajaroJugador/Scripts/Personaje.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Personaje : MonoBehaviour
+{
+    public float velMovimiento  =5.0f;
+    public float velRotacion = 200.0f;
+    public Animator anim;
+    public float DagnoPlayer=5;
+    public float x, y;
+    public Rigidbody rb;
+    public float jumpHeight=8f;
+    public bool isGrounded;
+    private float inicioDisparo;
+    public float tiempoDisparo;
+    public float VelDisparo;
+    public Transform lanzador;
+    public GameObject flechaPrefab;
+    public int Arco=0;
+    public int Comida=0;
+    public Text textoArco;
+    public Text textoComida;
+    public float angulo;
+    void Start() {
+        isGrounded=false;
+        textoArco.text= "Armas: "+Arco.ToString();
+        textoComida.text= "Comida: "+Comida.ToString();
+    }
+    void FixedUpdate() {
+            transform.Rotate(0,x*Time.deltaTime*velRotacion,0);
+            transform.Translate(0,0,y*Time.deltaTime*velMovimiento);
+            angulo = transform.eulerAngles.y;
+
+    }
+    void Update()
+    {    
+        x= Input.GetAxis("Horizontal");
+        y= Input.GetAxis("Vertical");
+
+        if(Input.GetMouseButtonDown(0) && isGrounded)
+        {
+            anim.SetTrigger("Golpe");
+            GameObject proyectilObjeto = Instantiate(flechaPrefab);
+            proyectilObjeto.transform.position = lanzador.position + lanzador.forward;
+            proyectilObjeto.transform.forward = lanzador.forward;
+        }
+        anim.SetFloat("VelX",x);
+        anim.SetFloat("VelY",y);
+
+
+
+        if (isGrounded){
+                if(Input.GetKeyDown(KeyCode.Space)){
+                    anim.SetBool("Jump",true);
+                    rb.AddForce(new Vector3(0,jumpHeight,0),ForceMode.Impulse);
+                }
+            anim.SetBool("Grounding",true);
+        }else{
+            Falling();
+        }
+    }
+    public void Falling(){
+        anim.SetBool("Jump",false);
+        anim.SetBool("Grounding",false);
+    }
+    void OnTriggerEnter(Collider otro){
+        if(otro.gameObject.CompareTag("Arco")){
+            otro.gameObject.SetActive(false);
+            Arco++;
+            DagnoPlayer=10;
+            cambiarTexto();
+        }
+        if(otro.gameObject.CompareTag("Comida")){
+            otro.gameObject.SetActive(false);
+            Comida++;
+            cambiarTexto();
+        }
+    }
+    void cambiarTexto(){
+            if(Comida>0){
+                textoComida.text= "Comida: "+(Comida).ToString();
+            }
+            if(Arco>0){
+                textoArco.text= "Armas: "+(Arco).ToString();
+            }
+
+    }
+}
