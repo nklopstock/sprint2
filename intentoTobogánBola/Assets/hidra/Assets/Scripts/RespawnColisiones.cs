08337cee330ca4d07dfa60b5faa862a9d45228d5
nklopstock <65245243+nklopstock@users.noreply.github.com>
2020-10-23 17:26:20 -0500

    Cambio 8

---
+++ intentoTobogánBola/Assets/hidra/Assets/Scripts/RespawnColisiones.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawnColisiones : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Jugador"))
+        {
+            Restart();
+        }
+        if(other.CompareTag("Proyectil"))
+        {
+            Destroy(other.gameObject);
+        }
+        
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
