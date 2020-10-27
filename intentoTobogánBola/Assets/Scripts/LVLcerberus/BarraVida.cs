1d27300768c7b191d844828cbf2a882c016c75a0
CarlosJPinto <72925993+CarlosJPinto@users.noreply.github.com>
2020-10-26 20:57:53 -0500

    Libro

---
+++ intentoTobogánBola/Assets/Scripts/LVLcerberus/BarraVida.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class BarraVida : MonoBehaviour
+{
+    public int vidaMAX;
+    public float vidaACTUAL;
+    public Image imagenBarraVida;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaACTUAL = vidaMAX;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RevisarVida();
+
+        if ( vidaACTUAL <=0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void RevisarVida()
+    {
+        imagenBarraVida.fillAmount = vidaACTUAL / vidaMAX;
+    }
+}
